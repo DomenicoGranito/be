2680c9c830eeabfc56380d9636fec0a73f8cf1fa
gargankit20 <gargankit936@gmail.com>
2018-07-16 11:18:02 +0530

    shadow and rounded image

--- Streamini/Controllers/CategoriesViewController.swift
+++ Streamini/Controllers/CategoriesViewController.swift
@@ -97,7 +97,15 @@ class CategoriesViewController: BaseViewController, PlayerViewControllerDelegate
         cell.videoYearLbl.text="\(video.year) | \(video.city)"
         cell.videoCategoryLbl.text=video.category
         cell.videoBrandLbl.text="#\(video.brand)"
+        
+        cell.userImageView.layer.borderColor=UIColor.white.cgColor
+        cell.userImageView.layer.masksToBounds=false
+        cell.userImageView.layer.shadowColor=UIColor.white.cgColor
+        cell.userImageView.layer.shadowRadius=2
+        cell.userImageView.layer.shadowOffset=CGSize(width:0, height:0)
+        cell.userImageView.layer.shadowOpacity=1
         cell.userImageView.sd_setImage(with:URL(string:"\(site)/uploads/\(video.user.id)-avatar.jpg"), placeholderImage:UIImage(named:"profile"))
+        
         cell.videoThumbnailImageView.sd_setImage(with:URL(string:"\(site)/thumb/\(video.id).jpg"), placeholderImage:UIImage(named:"videostream"))
         cell.likesAndCommentsCountLbl.text="\(video.likes) Likes • \(video.comments) Comments"
         
